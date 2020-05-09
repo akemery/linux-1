80a4b8f420371727f2a5957acc68795b22322bff
Emery Assogba <assogba.emery@gmail.com>
2020-05-10 01:02:34 +0200

    support for lfa nh_group add, delete, replace, handling dev  down and dev up

--- include/net/nexthop.h
+++ include/net/nexthop.h
@@ -85,9 +85,15 @@ struct nexthop {
 	struct net		*net;
 
 	u32			id;
+	u32			back_id;
+	u32			prin_id;
 	u8			protocol;   /* app managing this nh */
 	u8			nh_flags;
 	bool			is_group;
+	bool			is_back;
+	bool			is_prin;
+	bool			is_frr;
+	bool			frr_state;
 	refcount_t		refcnt;
 	struct rcu_head		rcu;
 
@@ -95,6 +101,7 @@ struct nexthop {
 		struct nh_info	__rcu *nh_info;
 		struct nh_group __rcu *nh_grp;
 	};
+	struct nh_info	__rcu *nh_info_back;
 };
 
 /* caller is holding rcu or rtnl; no reference taken to nexthop */
